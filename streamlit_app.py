f3226243d2686dfc72d21e1adbef929497fd621a
Charley Cai <caic@student.ubc.ca>
2024-12-02 12:12:08 +0000

    MIP implementation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,20 @@
 import streamlit as st
+import toml
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("UBC AI FLEX Advisor")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is UBC's Faculty of Medicine's virtual advisor for FLEX 419!"
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+# Secrets
+openai_api_key = st.secrets["flexAIToken"]
+assistantID = st.secrets["assistantID"]
+
+# Load prompt
+instructions = toml.load("./prompt.toml")["flexPrompt"]
+
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
@@ -30,27 +32,52 @@ else:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
+    # Create a OpenAI Thread
+    thread = client.beta.threads.create()
+
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("Type your question here!"):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
+        # Add message to thread
+        message = client.beta.threads.messages.create(
+            thread_id = thread.id,
+            role = "user",
+            content = prompt
+        )          
+
+        # Run the Thread
+        run = client.beta.threads.runs.create_and_poll(
+            thread_id=thread.id,
+            assistant_id = assistantID,
+            instructions = instructions
         )
 
+        if run.status == 'completed': 
+            messageResponse = client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
+            textResponse = messageResponse.data[0].content[0].text.value
+        else:
+            st.write(run.status)
+
+
+        # TODO: Implement streaming
+        
         # Stream the response to the chat using `st.write_stream`, then store it in 
         # session state.
         with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            # response = st.write_stream(stream)
+            st.markdown(textResponse)
+        st.session_state.messages.append({"role": "assistant", "content": textResponse})
+
+# footer
+footer_html = """<div style='text-align: center;'>
+  <p>Developed by Charley Cai MSI3 with StreamLit 🩺</p>
+</div>"""
+st.markdown(footer_html, unsafe_allow_html=True)
